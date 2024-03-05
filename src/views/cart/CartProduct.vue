5bc98412b384ca2f0e82cfc0411c28c5a56e56fe
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2024-03-06 00:09:34 +0700

    update

--- src/views/cart/CartProduct.vue
+++ src/views/cart/CartProduct.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="total-price">
-    <h1>$ 99999</h1>
+    <h1 class="a">Total Price :</h1>
+    <h1 class="b">$ 99999</h1>
   </div>
   <div class="product-container" v-for="product in products" :key="product.id">
     <div>
@@ -26,15 +27,32 @@ export default {
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Madimi+One&display=swap');
+
 h1 {
-  border-bottom: 1px solid #41b883;
+  font-family: 'Madimi One', sans-serif;
+}
+
+h1.a {
+  font-size: 20px;
   margin: 0;
   margin-top: 16px;
-  padding: 16px;
+  padding: 10px;
+}
+
+h1.b {
+  font-size: 24px;
+  margin: 0;
+  margin-top: 16px;
+  padding: 10px;
 }
 #total-price {
   padding: 16px;
   text-align: right;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #41b883;
 }
 
 .product-container {
@@ -46,8 +64,15 @@ h1 {
   justify-content: space-around;
 }
 .product-image {
+  --bg-color: #fff;
+  --main-color: #323232;
+  --main-focus: #2d8cf0;
   height: 100px;
   max-width: 100px;
+  border-radius: 10px;
+  background: var(--bg-color);
+  border: 2px solid var(--main-color);
+  box-shadow: 4px 4px var(--main-color);
 }
 
 button {
